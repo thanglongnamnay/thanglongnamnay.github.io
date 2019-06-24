ec572a29245df7bdec860e8f2e426bbb283f3af0
thanglongnamnay <thanglongnamnay@gmail.com>
2019-06-24 18:12:59 +0700

    show that everything is on dev

--- main.js
+++ main.js
@@ -1,27 +1,6 @@
-fetch(`https://graph.facebook.com/100003406357473/picture?type=large`)
-	.then(res => {
-		console.log(res);
-		const reader = res.body.getReader();
-		return new ReadableStream({
-	    start(controller) {
-	    	return pump();
-	    	function pump() {
-	    		return reader.read().then(({ done, value }) => {
-	    		// When no more data needs to be consumed, close the stream
-		    		if (done) {
-		     			controller.close();
-		    			return;
-		    		}
-		    		// Enqueue the next data chunk into our target stream
-		    		controller.enqueue(value);
-		    		return pump();
-		    	});
-		    }
-			}  
-		});
-	})
-	.then(stream => new Response(stream))
-	.then(response => response.blob())
-	.then(blob => URL.createObjectURL(blob))
-	.then(url => { image.src = url })
-	.catch(err => console.error(err));
+function makeLi(href, link, description) {
+	return `<li><a href="${ href }">${ link }</a> - ${ description }</li>`;
+}
+list = [
+	
+]
